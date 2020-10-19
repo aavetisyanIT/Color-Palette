70cc19ae351a0f1d40fcbc7549e88e6068e2ab04
aavetisyanIT <aavetisyanit@gmail.com>
2020-10-19 15:15:56 -0500

    App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
+class App extends Component{
+    render(){
+        return
+    }
+}
 
 export default App;
