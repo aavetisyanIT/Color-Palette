5b13fa49a7b42e4169761554726179e3d5eca549
aavetisyanIT <aavetisyanit@gmail.com>
2020-10-26 20:33:12 -0500

    Start Styling MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import MiniPalette from './MiniPalette';
+import { Link } from 'react-router-dom';
 
 export default class PaletteList extends Component {
-    render() {
-        const { palettes } = this.props
-        return (
-            <div>
-                <h1>React Colors</h1>
-                {palettes.map(palette => (
-                    <p>
-                        <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-                    </p>
-                ))}
-            </div>
-        )
-    }
+	render() {
+		const { palettes } = this.props;
+		return (
+			<div>
+				<MiniPalette />
+				<h1>React Colors</h1>
+				{palettes.map((palette) => (
+					<p>
+						<MiniPalette {...palette} />
+					</p>
+				))}
+			</div>
+		);
+	}
 }
