e3f121fac85c15b6d44e065b3c963cf69995b199
aavetisyan <aavetisyanit@gmail.com>
2020-11-02 09:16:45 -0600

    Display colors in SingleColorPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MiniPalette from './MiniPalette';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
 
 const styles = {
