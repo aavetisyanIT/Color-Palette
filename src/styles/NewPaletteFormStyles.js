3b782ce97e1c2ebf1c0c0c96bd677bfd6db85874
aavetisyan <aavetisyanit@gmail.com>
2020-11-08 19:55:22 -0600

    Tweak styles of form

--- src/styles/NewPaletteFormStyles.js
+++ src/styles/NewPaletteFormStyles.js
@@ -20,6 +20,7 @@ const styles = (theme) => ({
 		alignItems: 'center',
 	},
 	drawerHeader: {
+		width: '100%',
 		display: 'flex',
 		alignItems: 'center',
 		padding: '0 8px',
@@ -29,7 +30,7 @@ const styles = (theme) => ({
 	content: {
 		flexGrow: 1,
 		height: 'calc(100vh - 64px)',
-		padding: theme.spacing.unit * 3,
+		padding: 0,
 		transition: theme.transitions.create('margin', {
 			easing: theme.transitions.easing.sharp,
 			duration: theme.transitions.duration.leavingScreen,
