2663a6de992a413bb72f30c8cfeaf22f831d6075
aavetisyan <aavetisyanit@gmail.com>
2020-11-08 22:31:54 -0600

    Make form and navbar responsive

--- src/styles/PaletteFormNavStyles.js
+++ src/styles/PaletteFormNavStyles.js
@@ -1,4 +1,5 @@
 import { DRAWER_WIDTH } from '../constants';
+import sizes from './sizes';
 
 const drawerWidth = DRAWER_WIDTH;
 const styles = (theme) => ({
@@ -32,9 +33,16 @@ const styles = (theme) => ({
 	},
 	navBtns: {
 		marginRight: '1rem',
+		[sizes.down('xs')]: {
+			marginRight: '0.5rem',
+		},
 	},
 	button: {
 		margin: '0 0.5rem',
+		[sizes.down('xs')]: {
+			margin: '0 0.2rem',
+			padding: '0.2rem',
+		},
 	},
 	link: {
 		textDecoration: 'none',
