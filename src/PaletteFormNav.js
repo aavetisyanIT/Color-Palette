2746de345e5233fe3a76baa9c2bead0ea881969c
aavetisyan <aavetisyanit@gmail.com>
2020-11-08 18:44:59 -0600

    Fix Form Dialog and install imoji pickert

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -56,6 +56,7 @@ class PaletteFormNav extends Component {
 		this.state = { newPaletteName: '', formShowing: false };
 		this.handleChange = this.handleChange.bind(this);
 		this.showForm = this.showForm.bind(this);
+		this.hideForm = this.hideForm.bind(this);
 	}
 
 	handleChange(evt) {
@@ -65,6 +66,9 @@ class PaletteFormNav extends Component {
 	}
 	showForm() {
 		this.setState({ formShowing: true });
+	}
+	hideForm() {
+		this.setState({ formShowing: false });
 	}
 	render() {
 		const { classes, open, palettes, handleSubmit } = this.props;
@@ -119,6 +123,7 @@ class PaletteFormNav extends Component {
 					<PaletteMetaForm
 						palettes={palettes}
 						handleSubmit={handleSubmit}
+						hideForm={this.hideForm}
 					/>
 				)}
 			</div>
