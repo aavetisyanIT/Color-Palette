dcad663a7f4433ee30d6a1ce862f021d7aac0aa4
aavetisyan <aavetisyanit@gmail.com>
2020-11-10 10:52:09 -0600

    Prevent duplicate random colors

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -7,7 +7,7 @@ import styles from './styles/DraggableColorBoxStyles';
 const DraggableColorBox = SortableElement((props) => {
 	const { classes, handleClick, name, color } = props;
 	return (
-		<div className={props.classes.root} style={{ backgroundColor: color }}>
+		<div className={classes.root} style={{ backgroundColor: color }}>
 			<div className={classes.boxConent}>
 				<span> {name} </span>
 				<DeleteOutlinedIcon
