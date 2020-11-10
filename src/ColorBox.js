4c561071dcf26dbb67b850ea870944d2083bbb14
aavetisyan <aavetisyanit@gmail.com>
2020-11-10 09:38:46 -0600

    Fix navbar size and add classnames to ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,6 +3,7 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
 import styles from './styles/ColorBoxStyles';
 import { withStyles } from '@material-ui/styles';
+import classNames from 'classnames';
 
 class ColorBox extends Component {
 	constructor(props) {
@@ -29,14 +30,15 @@ class ColorBox extends Component {
 				<div style={{ background }} className={classes.ColorBox}>
 					<div
 						style={{ background }}
-						className={`${classes.copyOverlay} ${
-							copied && classes.showOverlay
-						}`}
+						className={classNames(classes.copyOverlay, {
+							[classes.showOverlay]: copied,
+						})}
 					/>
+
 					<div
-						className={`${classes.copyMessage} ${
-							copied && classes.showMessage
-						}`}
+						className={classNames(classes.copyMessage, {
+							[classes.showMessage]: copied,
+						})}
 					>
 						<h1>copied</h1>
 						<p className={classes.copyText}>{background}</p>
